d625d9f8dfa133223f993bdf282eb3fffd3f6cca
saka-21 <kasaikasai572@gmail.com>
2020-07-15 01:18:22 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+  var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue.js!',
+      count: 10,
+      user: {
+        lastName: 'Yamada',
+        firstName: 'Taiki',
+        prefecture: 'Tokyo'
+      },
+      colors: ['Red', 'Green', 'Blue']
+    }
+  })
